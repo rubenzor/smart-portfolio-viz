a581851d5e069cdf0bc8df4f10953cf710b99cb7
rubenzor <rubenr.fff@gmail.com>
2025-10-07 18:52:21 +0200

    "Cambio de estructura de proyecto: reorganización de módulos y adición de nuevos componentes"

--- src/smart_portfolio_viz/workflows/__init__.py
+++
@@ -1,9 +0,0 @@
-"""High level workflows that orchestrate data ingestion and analytics."""
-
-from .overview import OverviewConfig, PortfolioOverview, build_portfolio_overview
-
-__all__ = [
-    "OverviewConfig",
-    "PortfolioOverview",
-    "build_portfolio_overview",
-]
